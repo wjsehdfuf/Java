58adf0a88fec6b5ab8aed2206c668bdf55b1ba35
dongryeol.jeon <wpqmfdl12@naver.com>
2022-03-13 01:05:20 +0900

    [JAVA] LeapYear.java 생성
    해당 년도가 윤년인지에 대해 true 와 false 로 나타내는 코드

---
+++ src/LeapYear.java
@@ -0,0 +1,14 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 2024;
+		boolean isLeapYear;
+		
+		isLeapYear = (year % 4 == 0);
+		System.out.println(isLeapYear);
+
+	}
+
+}
