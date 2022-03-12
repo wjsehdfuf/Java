8c8011f9d6d777a8bf6e5b69345881cbc09c0afd
dongryeol.jeon <wpqmfdl12@naver.com>
2022-03-13 00:55:28 +0900

    [JAVA] AreaTest.java 생성
    원의 면적 구하는 코드 (PI를 상수값으로 즉 final 사용)

--- src/AreaTest.java
+++ src/AreaTest.java
@@ -3,7 +3,7 @@ public class AreaTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub\
 		double radius = 3.0;
-		double PI = 3.141592;
+		final double PI = 3.141592;
 		double area = radius * radius * PI;
 		
 		System.out.println("반지름이" + radius + "인 원의 면적은" + area + "입니다.");
