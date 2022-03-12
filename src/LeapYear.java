ed57e574fd63456a37c673a2d8ff417b7eecf30d
dongryeol.jeon <wpqmfdl12@naver.com>
2022-03-13 02:05:03 +0900

    [JAVA] LeapYear.java 와 QuadraticEq.java 생성
    input.close() 를 넣어 input을 닫음

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -1,14 +1,11 @@
-
 public class LeapYear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int year = 2024;
-		boolean isLeapYear;
+		boolean LeapYear;
 		
-		isLeapYear = (year % 4 == 0);
-		System.out.println(isLeapYear);
-
+		LeapYear = (year % 4 == 0);
+		System.out.println(LeapYear);
 	}
-
 }
--- src/QuadraticEq.java
+++ src/QuadraticEq.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
+
 public class QuadraticEq {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double b = -3.0;
-		double c = 2.0;
+		Scanner input = new Scanner(System.in);
+		
+		double b;
+		double c;
+		
+		System.out.print("b의 값을 입력하시오: ");
+		b = input.nextFloat();
+		
+		System.out.print("c의 값을 입력하시오: ");
+		c = input.nextFloat();
 		
 		double root = Math.sqrt(b * b - 4.0 * c);
 		
@@ -12,6 +22,7 @@ public class QuadraticEq {
 		
 		System.out.println("첫번째 근 : " + r1);
 		System.out.println("두번째 근 : " + r2);
+		input.close();
 
 
 	}
